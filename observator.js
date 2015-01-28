a20574ca8b1284218be334f0fab431772dd2e0da
andrii-frankiv <andrii.frnakiv@gmail.com>
2015-01-28 16:14:43 +0200

    simple ex.

--- observator.js
+++ observator.js
@@ -1,58 +1,139 @@
-var WeatherData = (function () {
-    function WeatherData() {
-        this.observers = [];
+/**
+ * OBSERVER
+ * Created by user on 27.01.15.
+ */
+//interface Subject {
+//    registerObserver(o: Observer): void;
+//    removeObserver(o: Observer): void;
+//    notifyObservers(): void;
+//}
+//
+//interface Observer {
+//    update(temp: number, humidity: number, pressure: number): void;
+//}
+//
+//interface DisplayElement {
+//    display(): void;
+//}
+//
+//class WeatherData implements Subject {
+//    private observers: Array<any>;
+//    private temperature: number;
+//    private humidity: number;
+//    private pressure: number;
+//
+//    constructor() {
+//        this.observers = [];
+//    }
+//
+//    registerObserver(o: Observer) {
+//        this.observers.push(o);
+//    }
+//
+//    removeObserver(o: Observer) {
+//        var i = this.observers.indexOf(o);
+//        if (i >= 0) {
+//            this.observers.splice(i);
+//        }
+//    }
+//
+//    notifyObservers() {
+//        for(var i = 0; i < this.observers.length; i++) {
+//            this.observers[i].update(this.temperature, this.humidity, this.pressure);
+//        }
+//    }
+//
+//    measurementsChanged() : void {
+//        this.notifyObservers();
+//    }
+//
+//    setMeasurments(temperature: number, humidity: number, pressure: number) {
+//        this.temperature = temperature;
+//        this.humidity = humidity;
+//        this.pressure = pressure;
+//        this.measurementsChanged();
+//    }
+//}
+//
+//class CurrentConditionDisplay implements Observer, DisplayElement {
+//    private temperature: number;
+//    private humidity: number;
+//    private pressure: number;
+//    private weatherData: Subject;
+//
+//    constructor(weatherData: Subject) {
+//        this.weatherData = weatherData;
+//        weatherData.registerObserver(this);
+//    }
+//
+//    update(temp: number, humidity: number, pressure: number) {
+//        this.temperature = temp;
+//        this.humidity = humidity;
+//        this.pressure = pressure;
+//        this.display();
+//    }
+//
+//    display() {
+//        console.log("Current conditions:" + " C "+ this.temperature + " |" + " % " + this.humidity + " | " + "Pa " + this.pressure);
+//    }
+//}
+//
+//class WeatherStation {
+//    static weatherData: WeatherData;
+//    static currentConditionDisplay: CurrentConditionDisplay;
+//    static init(): void {
+//        WeatherStation.weatherData = new WeatherData();
+//        WeatherStation.currentConditionDisplay = new CurrentConditionDisplay(WeatherStation.weatherData);
+//        WeatherStation.weatherData.setMeasurments(20,12, 3);
+//        WeatherStation.weatherData.setMeasurments(12,22, 13);
+//
+//    }
+//}
+//
+//WeatherStation.init();
+var PostOffice = (function () {
+    function PostOffice() {
+        this.observersList = [];
     }
-    WeatherData.prototype.registerObserver = function (o) {
-        this.observers.push(o);
+    PostOffice.prototype.registerObserver = function (o) {
+        this.observersList.push(o);
     };
-    WeatherData.prototype.removeObserver = function (o) {
-        var i = this.observers.indexOf(o);
-        if (i >= 0) {
-            this.observers.splice(i);
+    PostOffice.prototype.removeObserver = function (o) {
+        var i = this.observersList.indexOf(o);
+        if (i >= 0)
+            this.observersList.splice(i);
+    };
+    PostOffice.prototype.notifyObservers = function () {
+        for (var i = 0; i < this.observersList.length; i++) {
+            this.observersList[i].update(this.magazines);
         }
     };
-    WeatherData.prototype.notifyObservers = function () {
-        for (var i = 0; i < this.observers.length; i++) {
-            this.observers[i].update(this.temperature, this.humidity, this.pressure);
-        }
-    };
-    WeatherData.prototype.measurementsChanged = function () {
+    PostOffice.prototype.updateMagazinesList = function (list) {
+        this.magazines = list;
         this.notifyObservers();
     };
-    WeatherData.prototype.setMeasurments = function (temperature, humidity, pressure) {
-        this.temperature = temperature;
-        this.humidity = humidity;
-        this.pressure = pressure;
-        this.measurementsChanged();
-    };
-    return WeatherData;
+    return PostOffice;
 })();
-var CurrentConditionDisplay = (function () {
-    function CurrentConditionDisplay(weatherData) {
-        this.weatherData = weatherData;
-        weatherData.registerObserver(this);
+var Subscriber = (function () {
+    function Subscriber(observer) {
+        observer.registerObserver(this);
     }
-    CurrentConditionDisplay.prototype.update = function (temp, humidity, pressure) {
-        this.temperature = temp;
-        this.humidity = humidity;
-        this.pressure = pressure;
-        this.display();
+    Subscriber.prototype.showSubscriptions = function () {
+        console.log(this.magazines, " - magazines list");
     };
-    CurrentConditionDisplay.prototype.display = function () {
-        console.log("Current conditions:" + " C " + this.temperature + " |" + " % " + this.humidity + " | " + "Pa " + this.pressure);
+    Subscriber.prototype.update = function (list) {
+        this.magazines = list;
+        this.showSubscriptions();
     };
-    return CurrentConditionDisplay;
+    return Subscriber;
 })();
-var WeatherStation = (function () {
-    function WeatherStation() {
-    }
-    WeatherStation.init = function () {
-        WeatherStation.weatherData = new WeatherData();
-        WeatherStation.currentConditionDisplay = new CurrentConditionDisplay(WeatherStation.weatherData);
-        WeatherStation.weatherData.setMeasurments(20, 12, 3);
-        WeatherStation.weatherData.setMeasurments(12, 22, 13);
-    };
-    return WeatherStation;
-})();
-WeatherStation.init();
+function init() {
+    var list = ['RollingStones', 'Viva', 'Guardians'];
+    var mainOffice = new PostOffice();
+    var deliveryOffice = new Subscriber(mainOffice);
+    mainOffice.updateMagazinesList(list);
+    mainOffice.removeObserver(deliveryOffice);
+    mainOffice.updateMagazinesList(list);
+}
+init();
 //# sourceMappingURL=observator.js.map
--- observator.ts
+++ observator.ts
@@ -2,6 +2,96 @@
  * OBSERVER
  * Created by user on 27.01.15.
  */
+//interface Subject {
+//    registerObserver(o: Observer): void;
+//    removeObserver(o: Observer): void;
+//    notifyObservers(): void;
+//}
+//
+//interface Observer {
+//    update(temp: number, humidity: number, pressure: number): void;
+//}
+//
+//interface DisplayElement {
+//    display(): void;
+//}
+//
+//class WeatherData implements Subject {
+//    private observers: Array<any>;
+//    private temperature: number;
+//    private humidity: number;
+//    private pressure: number;
+//
+//    constructor() {
+//        this.observers = [];
+//    }
+//
+//    registerObserver(o: Observer) {
+//        this.observers.push(o);
+//    }
+//
+//    removeObserver(o: Observer) {
+//        var i = this.observers.indexOf(o);
+//        if (i >= 0) {
+//            this.observers.splice(i);
+//        }
+//    }
+//
+//    notifyObservers() {
+//        for(var i = 0; i < this.observers.length; i++) {
+//            this.observers[i].update(this.temperature, this.humidity, this.pressure);
+//        }
+//    }
+//
+//    measurementsChanged() : void {
+//        this.notifyObservers();
+//    }
+//
+//    setMeasurments(temperature: number, humidity: number, pressure: number) {
+//        this.temperature = temperature;
+//        this.humidity = humidity;
+//        this.pressure = pressure;
+//        this.measurementsChanged();
+//    }
+//}
+//
+//class CurrentConditionDisplay implements Observer, DisplayElement {
+//    private temperature: number;
+//    private humidity: number;
+//    private pressure: number;
+//    private weatherData: Subject;
+//
+//    constructor(weatherData: Subject) {
+//        this.weatherData = weatherData;
+//        weatherData.registerObserver(this);
+//    }
+//
+//    update(temp: number, humidity: number, pressure: number) {
+//        this.temperature = temp;
+//        this.humidity = humidity;
+//        this.pressure = pressure;
+//        this.display();
+//    }
+//
+//    display() {
+//        console.log("Current conditions:" + " C "+ this.temperature + " |" + " % " + this.humidity + " | " + "Pa " + this.pressure);
+//    }
+//}
+//
+//class WeatherStation {
+//    static weatherData: WeatherData;
+//    static currentConditionDisplay: CurrentConditionDisplay;
+//    static init(): void {
+//        WeatherStation.weatherData = new WeatherData();
+//        WeatherStation.currentConditionDisplay = new CurrentConditionDisplay(WeatherStation.weatherData);
+//        WeatherStation.weatherData.setMeasurments(20,12, 3);
+//        WeatherStation.weatherData.setMeasurments(12,22, 13);
+//
+//    }
+//}
+//
+//WeatherStation.init();
+
 interface Subject {
     registerObserver(o: Observer): void;
     removeObserver(o: Observer): void;
@@ -9,85 +99,67 @@ interface Subject {
 }
 
 interface Observer {
-    update(temp: number, humidity: number, pressure: number): void;
+    update(list: string[]): void
 }
 
-interface DisplayElement {
-    display(): void;
+interface ShowSubscriptions {
+    showSubscriptions(): void;
 }
 
-class WeatherData implements Subject {
-    private observers: Array<any>;
-    private temperature: number;
-    private humidity: number;
-    private pressure: number;
+class PostOffice implements Subject {
+    private observersList: Observer[];
+    private magazines: string[];
 
     constructor() {
-        this.observers = [];
+        this.observersList = [];
     }
 
     registerObserver(o: Observer) {
-        this.observers.push(o);
+        this.observersList.push(o);
     }
 
     removeObserver(o: Observer) {
-        var i = this.observers.indexOf(o);
-        if (i >= 0) {
-            this.observers.splice(i);
-        }
+        var i = this.observersList.indexOf(o);
+        if (i >= 0) this.observersList.splice(i);
     }
 
     notifyObservers() {
-        for(var i = 0; i < this.observers.length; i++) {
-            this.observers[i].update(this.temperature, this.humidity, this.pressure);
+        for (var i = 0; i < this.observersList.length; i ++) {
+            this.observersList[i].update(this.magazines);
         }
     }
 
-    measurementsChanged() : void {
+    updateMagazinesList(list: string[]) {
+        this.magazines = list;
         this.notifyObservers();
     }
 
-    setMeasurments(temperature: number, humidity: number, pressure: number) {
-        this.temperature = temperature;
-        this.humidity = humidity;
-        this.pressure = pressure;
-        this.measurementsChanged();
+}
+
+class Subscriber implements Observer, ShowSubscriptions {
+    private magazines: string[];
+
+    constructor( observer: PostOffice) {
+        observer.registerObserver(this);
+    }
+
+    showSubscriptions() {
+        console.log(this.magazines, " - magazines list");
+    }
+
+    update(list: string[]) {
+        this.magazines = list;
+        this.showSubscriptions();
     }
 }
 
-class CurrentConditionDisplay implements Observer, DisplayElement {
-    private temperature: number;
-    private humidity: number;
-    private pressure: number;
-    private weatherData: Subject;
-
-    constructor(weatherData: Subject) {
-        this.weatherData = weatherData;
-        weatherData.registerObserver(this);
-    }
-
-    update(temp: number, humidity: number, pressure: number) {
-        this.temperature = temp;
-        this.humidity = humidity;
-        this.pressure = pressure;
-        this.display();
-    }
-
-    display() {
-        console.log("Current conditions:" + " C "+ this.temperature + " |" + " % " + this.humidity + " | " + "Pa " + this.pressure);
-    }
+function init() {
+    var list = ['RollingStones', 'Viva', 'Guardians'];
+    var mainOffice = new PostOffice();
+    var deliveryOffice = new Subscriber(mainOffice);
+    mainOffice.updateMagazinesList(list);
+    mainOffice.removeObserver(deliveryOffice);
+    mainOffice.updateMagazinesList(list);
 }
 
-class WeatherStation {
-    static weatherData: WeatherData;
-    static currentConditionDisplay: CurrentConditionDisplay;
-    static init(): void {
-        WeatherStation.weatherData = new WeatherData();
-        WeatherStation.currentConditionDisplay = new CurrentConditionDisplay(WeatherStation.weatherData);
-        WeatherStation.weatherData.setMeasurments(20,12, 3);
-        WeatherStation.weatherData.setMeasurments(12,22, 13);
-
-    }
-}
-
-WeatherStation.init();
+init();
